77d7f0b0414f355154491a129076d87dc5c44742
dpdahal <dp48dahal@gmail.com>
2024-02-29 10:50:01 +0545

    test

--- api/controller/LoginController.js
+++ api/controller/LoginController.js
@@ -1,9 +1,21 @@
+import User from "../models/User.js";
 
 class LoginController{
    
-    login(req, res){
-        
-        return res.json({message: 'Login successfull'});
+    async login(req, res){
+        let {email, password} = req.body;
+        let findData = await User.findOne({email: email});
+        if(!findData){
+            return res.status(400).json({message: 'Invalid credentials'});
+        }else{
+            let isMatch = await findData.comparePassword(password);
+            if(!isMatch){
+                return res.status(400).json({message: 'Invalid credentials'});
+            }else{
+                let token = findData.generateToken();
+                return res.json({token: token});
+            }
+        }
     }
 
 }
